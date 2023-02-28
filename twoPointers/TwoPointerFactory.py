cb9bac3e876eed9dd92c6fee3d8e524fc850ce00
Subrato Mondal <subrato.mondal18@gmail.com>
2023-02-28 10:03:28 +0530

    updated

--- twoPointers/TwoPointerFactory.py
+++ twoPointers/TwoPointerFactory.py
@@ -3,9 +3,11 @@ from . import ArrangeChar
 from . import ThreeSumClosest
 from . import FourSum
 from . import RemoveDuplicatesFromSortedArray
+from . import TrappingRainWater
 
 class Factory(enum.Enum):
     ARRANGE_CHAR = ArrangeChar.Problem
     THREE_SUM_CLOSEST = ThreeSumClosest.Problem
     FOUR_SUM = FourSum.Problem
     REMOVE_DUP_FRM_SORTED_ARRAY = RemoveDuplicatesFromSortedArray.Problem
+    TOTAL_TRAPPED_RAINWATER = TrappingRainWater.Problem
